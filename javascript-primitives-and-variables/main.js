e5f75715f3de094ec5396c5e8ad3818cd2c48925
Yessenia Rodriguez <yessi.rod30@gmail.com>
2021-01-25 10:59:52 -0800

    Completed js primitives and variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,20 @@
+var fullName = 'Yessenia Rodriguez';
+console.log('fullName:', fullName);
+
+var isCool = true;
+console.log('isCool:', isCool);
+
+var totalPets = 1;
+console.log('totalPets:', totalPets);
+
+console.log('typeof fullName:', typeof fullName);
+console.log('typeof isCool:', typeof isCool);
+console.log('typeof totalPets:', typeof totalPets);
+
+var unknown = null;
+console.log('unknown:', unknown);
+console.log('typeof unknown:', typeof unknown);
+
+var never;
+console.log('never:', never);
+console.log('typeof never:', typeof never);
