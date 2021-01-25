7d3644c87b2005b9f5ec25072d26174ed41f6fec
Yessenia Rodriguez <yessi.rod30@gmail.com>
2021-01-25 12:41:27 -0800

    Completed js operators and expressions

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,43 @@
+var width = 10;
+var height = 8;
+
+var area = width * height;
+console.log('area:', area);
+console.log('typeof area:', typeof area);
+
+var bill = 100;
+var payment = 200;
+
+var change = payment - bill;
+console.log('change:', change);
+console.log('typeof change:', typeof change);
+
+var quizzes = 88;
+var midterm = 90;
+var final = 94;
+
+var grade = (quizzes + midterm + final) / 3;
+console.log('grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+var firstName = 'Yessenia ';
+var lastName = 'Rodriguez';
+
+var fullName = firstName + lastName;
+console.log('fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+var pH = 6;
+var isAcidic = pH < 7;
+console.log('isAcidic:', isAcidic);
+console.log('typeof isAcidic', typeof isAcidic);
+
+var headCount = 11;
+var isSparta = headCount === 300;
+console.log('isSparta:', isSparta);
+console.log('typeof isSparta', typeof isSparta);
+
+var motto = fullName;
+motto = fullName + ' is the GOAT';
+console.log('motto:', motto);
+console.log('typeof motto', typeof motto);
