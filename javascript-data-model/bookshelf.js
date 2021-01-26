8bcbf19db0626c56162339989883303f07821bc9
Yessenia Rodriguez <yessi.rod30@gmail.com>
2021-01-26 11:32:44 -0800

    Completed js data model

---
+++ javascript-data-model/bookshelf.js
@@ -0,0 +1,21 @@
+var bookshelf = [
+  {
+    isbn: '978-1491929483',
+    title: 'Introduction to JavaScript Object Notation: A To-the-Point Guide to JSON',
+    author: 'Lindsay Bassett'
+  },
+  {
+    isbn: '978-1484218624',
+    title: 'JSON Quick Syntax Reference',
+    author: 'Wallace Jackson'
+  },
+  {
+    isbn: '978-0692232699',
+    title: 'Build APIs You Won\'t Hate: Everyone and their dog wants an API, so you should probably learn how to build them',
+    author: 'Phil Sturgeon & Laura Bohill'
+  }
+];
+
+console.log('Book #2 author:', bookshelf[1].author);
+console.log('Book #1 ISBN:', bookshelf[0].isbn);
+console.log('Book #3 title:', bookshelf[2].title);
---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,78 @@
+var orderHistory = [
+  {
+    orderPlaced: 'August 4, 2020',
+    total: '$34.00',
+    shipTo: 'JS Masher',
+    orderNumber: '114-3941689-8772232',
+    deliveryDate: 'Delivered Aug 8, 2020',
+    message: '',
+    title: 'JavaScript for impatient programmers',
+    author: 'Rauschmayer, Dr. Axel',
+    return: 'Return window closed on Sep 7, 2020',
+    cost: '$31.55',
+    orderDetails: '',
+    invoice: ''
+  },
+
+  {
+    orderPlaced: 'July 19, 2020',
+    total: '$44.53',
+    shipTo: 'JS Masher',
+    orderNumber: '113-9984268-1280257',
+    deliveryDate: 'Delivered Jul 20, 2020',
+    message: 'Your package was delivered. It was handed directly to a resident.',
+    title: 'The Timeless Way of Building',
+    author: 'Alexander, Christopher',
+    return: 'Return window closed on Aug 19, 2020',
+    cost: '$41.33',
+    orderDetails: '',
+    invoice: ''
+  },
+
+  {
+    orderPlaced: 'July 4, 2020',
+    total: '$17.22',
+    shipTo: 'JS Masher',
+    orderNumber: '114-2875557-9059409',
+    deliveryDate: 'Delivered Jul 7, 2020',
+    message: 'Your package was delivered. It was handed directly to a resident.',
+    title: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+    author: '',
+    return: 'Return window closed on Aug 5, 2020',
+    cost: '$15.98',
+    orderDetails: '',
+    invoice: ''
+
+  },
+
+  {
+    orderPlaced: 'July 3, 2020',
+    total: '$138.93',
+    shipTo: 'JS Masher',
+    orderNumber: '113-2883177-2648248',
+    deliveryDate: 'Delivered Jul 5, 2020',
+    message: '',
+    title: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+    author: '',
+    return: 'Return window closed on Aug 4, 2020',
+    cost: '$94.95',
+    orderDetails: '',
+    invoice: ''
+  },
+
+  {
+    orderPlaced: 'July 3, 2020',
+    total: '$138.93',
+    shipTo: 'JS Masher',
+    orderNumber: '113-2883177-2648248',
+    deliveryDate: 'Delivered Jul 5, 2020',
+    message: '',
+    title: 'The Art of Sql',
+    author: 'Faroult, Stephane',
+    return: 'Return window closed on Aug 4, 2020',
+    cost: '$33.99',
+    orderDetails: '',
+    invoice: ''
+  }
+
+];
