6f2cdcca8d5fef331d60df788e5a97bace4d6b56
Yessenia Rodriguez <yessi.rod30@gmail.com>
2021-01-25 17:33:58 -0800

    Completed js arrays

---
+++ javascript-arrays/main.js
@@ -0,0 +1,20 @@
+var colors = ['red', 'white', 'blue'];
+console.log('colors[0]:', colors[0]);
+console.log('colors[1]:', colors[1]);
+console.log('colors[2]:', colors[2]);
+
+console.log('America is ' + colors[0] + ', ' + colors[1] + ', and ' + colors[2]);
+
+colors[2] = 'green';
+console.log('Mexico is ' + colors[0] + ', ' + colors[1] + ', and ' + colors[2]);
+
+console.log('colors:', colors);
+
+var students = ['Jordan', 'Gabby', 'Barbara', 'Marz'];
+var numberOfStudents = students.length;
+
+console.log('There are ' + numberOfStudents + ' students in the class.');
+var lastStudent = students[3];
+
+console.log('The last student in the array is ' + lastStudent + '.');
+console.log('students:', students);
