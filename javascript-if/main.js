3d12d8e27f1a2a1b99dc99e45390e9e404df07b9
Yessenia Rodriguez <yessi.rod30@gmail.com>
2021-01-27 16:22:40 -0800

    Completed javascript if

--- javascript-if/main.js
+++ javascript-if/main.js
@@ -51,8 +51,6 @@ function isOldEnoughToDrive(person) {
 function isOldEnoughToDrinkAndDrive(person) {
   if (person.age >= 21 && person.age >= 16) {
     return false;
-  } else {
-    return true;
   }
 }
 
