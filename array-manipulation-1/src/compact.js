833a543d816c8e94d08571205933555897bd29cd
Yessenia Rodriguez <yessi.rod30@gmail.com>
2021-01-30 15:31:11 -0800

    removed else statement from compact function

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -4,8 +4,6 @@ function compact(array) {
   for (var i = 0; i < array.length; i++) {
     if (Boolean(array[i]) === true) {
       newArray.push(array[i]);
-    } else {
-      delete array[i];
     }
   }
   return newArray;
