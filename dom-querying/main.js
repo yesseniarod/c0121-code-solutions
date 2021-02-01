a4423846790f61de010f5d7c2e60676e89e954fe
Yessenia Rodriguez <yessi.rod30@gmail.com>
2021-02-01 10:58:15 -0800

    Completed dom-querying

---
+++ dom-querying/main.js
@@ -0,0 +1,19 @@
+console.log('hello, world');
+
+var h1 = document.querySelector('h1');
+console.log('h1:', h1);
+console.dir(h1);
+
+var explanation = document.querySelector('#explanation');
+console.log('explanation:', explanation);
+console.dir(explanation);
+
+var hint = document.querySelector('.hint');
+console.log('hint:', hint);
+console.dir(hint);
+
+var elementP = document.querySelectorAll('p');
+console.log('elementP:', elementP);
+
+var exampleLink = document.querySelectorAll('.example-link');
+console.log('exampleLink:', exampleLink);
