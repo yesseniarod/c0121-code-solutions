14af04ff4dc2a406ee63fe407a58a6c23bb15261
Yessenia Rodriguez <yessi.rod30@gmail.com>
2021-02-02 12:28:49 -0800

    Completed javascript-light-bulb

---
+++ javascript-light-bulb/main.js
@@ -0,0 +1,16 @@
+var clickCounter = 0;
+var $lightBulb = document.querySelector('.light-bulb');
+var $container = document.querySelector('.container');
+
+function lightOnAndOff(event) {
+  clickCounter++;
+  if (clickCounter % 2 !== 0) {
+    $lightBulb.className = 'buttonClick';
+    $container.className = 'lightOn';
+  } else if (clickCounter % 2 === 0) {
+    $lightBulb.className = 'light-bulb';
+    $container.className = 'container';
+  }
+}
+
+$lightBulb.addEventListener('click', lightOnAndOff);
