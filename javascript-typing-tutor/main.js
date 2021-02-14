1a3a2ca516b695baf5606c9086de04c4b6fb3bfb
Yessenia Rodriguez <yessi.rod30@gmail.com>
2021-02-14 10:27:41 -0800

    bug user unable to attempt retry

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,22 +1,52 @@
 var text = 'grumpy wizards make toxic brew';
 var $tutor = document.querySelector('.tutor');
-var characters = text.split('');
-var $span = document.querySelectorAll('span');
+var characters = text.split('').map(function (char) {
+  var span = document.createElement('span');
+  span.textContent = char;
+  $tutor.appendChild(span);
+  return span;
+});
 
-function typeTutor({ key }) {
-  for (var i = 0; i < characters.length; i++) {
-    var currentCharacter = characters[i];
-    $span[i].className = 'cursor';
+var i = 0;
+var currentCharacter = characters[i];
+currentCharacter.classList.add('cursor');
 
-    if (key === currentCharacter) {
-      $span[i].className = 'correct';
+// var $span = document.querySelectorAll('span');
 
-    } else if (key !== currentCharacter) {
-      $span[i].className = 'incorrect';
-    }
+// function typeTutor({ key }) {
+//   for (var i = 0; i < characters.length; i++) {
+//     var currentCharacter = characters[i];
+//     // $span.removeAttribute('class');
 
+//     if (key === currentCharacter) {
+//       $span[i].className = 'correct';
+//       // $span[i].setAttribute('class', 'cursor');
+
+//     } else if (key !== currentCharacter) {
+//       $span.className = 'incorrect';
+//     }
+
+//   }
+// }
+
+document.addEventListener('keydown', function ({ key }) {
+  // console.log(key);
+  if (key === currentCharacter.textContent) {
+    // typed correct key
+    currentCharacter.classList.remove('cursor');
+    currentCharacter.classList.add('correct');
+    currentCharacter = characters[++i];
+    currentCharacter.classList.add('cursor');
+  } else if (key !== currentCharacter.textContent) {
+    // currentCharacter.classList.remove('cursor');
+    // currentCharacter.classList.add('incorrect');
+    currentCharacter.classList.add('cursor');
+    currentCharacter.classList.add('incorrect');
+    // currentCharacter.removeAttribute('class');
+    // currentCharacter.classList.remove('cursor');
+
+    // currentCharacter = characters[++i];
+    // currentCharacter.classList.add('cursor');
   }
-}
-
-document.addEventListener('keydown', typeTutor);
-$tutor.addEventListener('keydown', typeTutor);
+});
+// $tutor.addEventListener('keydown', typeTutor);
