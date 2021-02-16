5248bcc35e1efb8ed69f47a5b4ffde5d4c88801a
Yessenia Rodriguez <yessi.rod30@gmail.com>
2021-02-16 12:19:29 -0800

    taking images of errors

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -2,14 +2,14 @@ var text = 'grumpy wizards make toxic brew';
 var $tutor = document.querySelector('.tutor');
 var characters = text.split('');
 // .map(function (char) {
-//   var span = document.createElement('span');
-//   span.textContent = char;
-//   $tutor.appendChild(span);
-//   return span;
-// });
+//    var span = document.createElement('span');
+//    span.textContent = char;
+//    $tutor.appendChild(span);
+//    return span;
+//  });
 
-// var i = 0;
-// var currentCharacter = characters[i];
+//  var i = 0;
+//  var currentCharacter = characters[i];
 // currentCharacter.classList.add('cursor');
 // console.log(characters);
 
@@ -20,8 +20,8 @@ function typeTutor(event) {
   //  console.log(key);
   for (var i = 0; i < characters.length; i++) {
     var currentCharacter = characters[i];
-    // $span[i].className = 'cursor';
-    // characters[currentCharacter] = 'cursor';
+    $span[i].className = 'cursor';
+    characters[currentCharacter] = 'cursor';
 
     if (key === currentCharacter) {
       $span[i].className = 'correct';
@@ -34,25 +34,25 @@ function typeTutor(event) {
   }
 }
 
-// document.addEventListener('keydown', function ({ key }) {
-//   // console.log(key);
-//   if (key === currentCharacter.textContent) {
-//     // typed correct key
-//     currentCharacter.classList.remove('cursor');
-//     currentCharacter.classList.add('correct');
-//     currentCharacter = characters[++i];
-//     currentCharacter.classList.add('cursor');
-//   } else if (key !== currentCharacter.textContent) {
-//     // currentCharacter.classList.remove('cursor');
-//     // currentCharacter.classList.add('incorrect');
-//     currentCharacter.classList.add('cursor');
-//     currentCharacter.classList.add('incorrect');
-//     // currentCharacter.removeAttribute('class');
-//     // currentCharacter.classList.remove('cursor');
+//  document.addEventListener('keydown', function ({ key }) {
+//    console.log(key);
+//    if (key === currentCharacter.textContent) {
+//      // typed correct key
+//      currentCharacter.classList.remove('cursor');
+//      currentCharacter.classList.add('correct');
+//      currentCharacter = characters[++i];
+//      currentCharacter.classList.add('cursor');
+//    } else if (key !== currentCharacter.textContent) {
+//       currentCharacter.classList.remove('cursor');
+//       currentCharacter.classList.add('incorrect');
+//      currentCharacter.classList.add('cursor');
+//      currentCharacter.classList.add('incorrect');
+//       currentCharacter.removeAttribute('class');
+//      currentCharacter.classList.remove('cursor');
 
-//     // currentCharacter = characters[++i];
-//     // currentCharacter.classList.add('cursor');
+//      currentCharacter = characters[++i];
+//       currentCharacter.classList.add('cursor');
 //   }
-// });
+//  });
 document.addEventListener('keydown', typeTutor);
 $tutor.addEventListener('keydown', typeTutor);
