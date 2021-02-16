775da0a24db6688f6af5c20a7241db58f31ad198
Yessenia Rodriguez <yessi.rod30@gmail.com>
2021-02-15 16:06:51 -0800

    made changes to sumAll and average methods

--- javascript-custom-methods/calculator.js
+++ javascript-custom-methods/calculator.js
@@ -22,29 +22,20 @@ var calculator = {
   },
 
   sumAll: function (numbers) {
-    var i = 0;
-    var total;
-    if (numbers.length === 2) {
-      total = numbers[i++] + numbers[i];
-    } else if (numbers.length === 3) {
-      total = numbers[i++] + numbers[i++] + numbers[i++];
-    } else if (numbers.length === 4) {
-      total = numbers[i++] + numbers[i++] + numbers[i++] + numbers[i++];
+    var total = 0;
+    for (var i = 0; i < numbers.length; i++) {
+      total += numbers[i];
     }
     return total;
 
   },
 
   getAverage: function (numbers) {
-    var j = 0;
-    var average;
-    if (numbers.length === 2) {
-      average = (numbers[j++] + numbers[j]) / numbers.length;
-    } else if (numbers.length === 3) {
-      average = (numbers[j++] + numbers[j++] + numbers[j++]) / numbers.length;
-    } else if (numbers.length === 4) {
-      average = (numbers[j++] + numbers[j++] + numbers[j++] + numbers[j++]) / numbers.length;
+    var average = 0;
+    for (var j = 0; j < numbers.length; j++) {
+      average += numbers[j];
     }
-    return average;
+
+    return average / numbers.length;
   }
 };
