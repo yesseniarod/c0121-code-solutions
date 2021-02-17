447b479e5f08897cb4af0e71ec2dbaa64da97999
Yessenia Rodriguez <yessi.rod30@gmail.com>
2021-02-17 08:45:51 -0800

    in progress

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,6 +1,33 @@
 var text = 'grumpy wizards make toxic brew';
 var $tutor = document.querySelector('.tutor');
-var characters = text.split('');
+
+function createSpan(event) {
+  for (var i = 0; i < text.length; i++) {
+    var $span = document.createElement('span');
+    $span.textContent = text[i];
+    $tutor.append($span);
+
+  }
+}
+
+var index = 0;
+var currentCharacter = text[index];
+// console.log(currentCharacter);
+// prints out g
+
+function checkKeys(event) {
+  var key = event.key;
+  for (var j = 0; j < text.length; j++) {
+    // currentCharacter = text[j];
+    if (key === currentCharacter) {
+    //  console.log('correct');
+    //  console.log(currentCharacter);
+    }
+    // else if (key !== currentCharacter)
+  }
+}
+
+// var characters = text.split('');
 // .map(function (char) {
 //    var span = document.createElement('span');
 //    span.textContent = char;
@@ -8,31 +35,26 @@ var characters = text.split('');
 //    return span;
 //  });
 
-//  var i = 0;
-//  var currentCharacter = characters[i];
-// currentCharacter.classList.add('cursor');
-// console.log(characters);
+// var $span = document.querySelectorAll('span');
 
-var $span = document.querySelectorAll('span');
+// function typeTutor(event) {
+//   var key = event.key;
+//   //  console.log(key);
+//   for (var i = 0; i < characters.length; i++) {
+//     var currentCharacter = characters[i];
+//     $span[i].className = 'cursor';
+//     characters[currentCharacter] = 'cursor';
 
-function typeTutor(event) {
-  var key = event.key;
-  //  console.log(key);
-  for (var i = 0; i < characters.length; i++) {
-    var currentCharacter = characters[i];
-    $span[i].className = 'cursor';
-    characters[currentCharacter] = 'cursor';
+//     if (key === currentCharacter) {
+//       $span[i].className = 'correct';
+//       $span[i].classList.remove('cursor');
 
-    if (key === currentCharacter) {
-      $span[i].className = 'correct';
-      $span[i].classList.remove('cursor');
+//     } else if (key !== currentCharacter) {
+//       $span.className = 'incorrect';
+//     }
 
-    } else if (key !== currentCharacter) {
-      $span.className = 'incorrect';
-    }
-
-  }
-}
+//   }
+// }
 
 //  document.addEventListener('keydown', function ({ key }) {
 //    console.log(key);
@@ -54,5 +76,6 @@ function typeTutor(event) {
 //       currentCharacter.classList.add('cursor');
 //   }
 //  });
-document.addEventListener('keydown', typeTutor);
-$tutor.addEventListener('keydown', typeTutor);
+window.addEventListener('load', createSpan);
+document.addEventListener('keydown', checkKeys);
+// document.addEventListener('keydown', checkKeys);
