866b1a271f08f4af9fcb66cac68509950880c937
yesseniarod <yessi.rod30@gmail.com>
2021-02-20 02:18:40 +0000

    made changes to condition statements

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -12,13 +12,13 @@ var clickBack = 0;
 
 $next.addEventListener('click', function (event) {
   clickCounter++;
-  if (clickCounter > 0 && clickCounter <= 1) {
+  if (clickCounter === 1) {
     $currentImage.setAttribute('src', './images/004.png');
-  } else if (clickCounter > 1 && clickCounter <= 2) {
+  } else if (clickCounter === 2) {
     $currentImage.setAttribute('src', './images/007.png');
-  } else if (clickCounter > 2 && clickCounter <= 3) {
+  } else if (clickCounter === 3) {
     $currentImage.setAttribute('src', './images/025.png');
-  } else if (clickCounter > 3 && clickCounter <= 4) {
+  } else if (clickCounter === 4) {
     $currentImage.setAttribute('src', './images/039.png');
   } else {
     $currentImage.setAttribute('src', './images/001.png');
@@ -28,13 +28,13 @@ $next.addEventListener('click', function (event) {
 
 $previous.addEventListener('click', function (event) {
   clickBack++;
-  if (clickBack > 0 && clickBack <= 1) {
+  if (clickBack === 1) {
     $currentImage.setAttribute('src', './images/039.png');
-  } else if (clickBack > 1 && clickBack <= 2) {
+  } else if (clickBack === 2) {
     $currentImage.setAttribute('src', './images/025.png');
-  } else if (clickBack > 2 && clickBack <= 3) {
+  } else if (clickBack === 3) {
     $currentImage.setAttribute('src', './images/007.png');
-  } else if (clickBack > 3 && clickBack <= 4) {
+  } else if (clickBack === 4) {
     $currentImage.setAttribute('src', './images/004.png');
   } else {
     $currentImage.setAttribute('src', './images/001.png');
@@ -72,5 +72,4 @@ function changeImage() {
     $currentImage.setAttribute('src', imageSlider[0]);
     clearInterval(imageId);
   }
-
 }
