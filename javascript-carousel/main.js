8ff113efe9bed7ff926ce93b306ef4cb458a0c29
yesseniarod <yessi.rod30@gmail.com>
2021-03-10 06:15:41 +0000

    made requested changes and used event delegation.

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,11 +1,7 @@
 var $next = document.querySelector('.next');
 var $previous = document.querySelector('.previous');
 var $currentImage = document.querySelector('.first');
-var $radio1 = document.querySelector('.dot1');
-var $radio2 = document.querySelector('.dot2');
-var $radio3 = document.querySelector('.dot3');
-var $radio4 = document.querySelector('.dot4');
-var $radio5 = document.querySelector('.dot5');
+var $dotContainer = document.querySelector('.dots');
 
 var clickCounter = 0;
 var clickBack = 0;
@@ -41,21 +37,19 @@ $previous.addEventListener('click', function (event) {
   }
 });
 
-$radio1.addEventListener('click', function (event) {
-  $currentImage.setAttribute('src', './images/001.png');
-});
 
-$radio2.addEventListener('click', function (event) {
-  $currentImage.setAttribute('src', './images/004.png');
-});
-$radio3.addEventListener('click', function (event) {
-  $currentImage.setAttribute('src', './images/007.png');
-});
-$radio4.addEventListener('click', function (event) {
-  $currentImage.setAttribute('src', './images/025.png');
-});
-$radio5.addEventListener('click', function (event) {
-  $currentImage.setAttribute('src', './images/039.png');
+$dotContainer.addEventListener('click', (event) => {
+  if (event.target && event.target.matches('input.dot1')) {
+    $currentImage.setAttribute('src', './images/001.png');
+  } else if (event.target && event.target.matches('input.dot2')) {
+    $currentImage.setAttribute('src', './images/004.png');
+  } else if (event.target && event.target.matches('input.dot3')) {
+    $currentImage.setAttribute('src', './images/007.png');
+  } else if (event.target && event.target.matches('input.dot4')) {
+    $currentImage.setAttribute('src', './images/025.png');
+  } else if (event.target && event.target.matches('input.dot5')) {
+    $currentImage.setAttribute('src', './images/039.png');
+  }
 });
 
 var imageSlider = ['./images/001.png', './images/004.png', './images/007.png', './images/025.png', './images/039.png'];
