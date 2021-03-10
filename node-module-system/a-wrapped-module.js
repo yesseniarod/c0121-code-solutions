6a505bbb0c7186025d97059c03e10c68c5dbbde9
yesseniarod <yessi.rod30@gmail.com>
2021-03-10 18:55:38 +0000

    Completed node-module-system

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,14 @@
+console.log('typeof exports:', typeof exports);
+console.log('exports value:', exports);
+
+console.log('typeof require:', typeof require);
+console.log('require value:', require);
+
+console.log('typeof module:', typeof module);
+console.log('module value:', module);
+
+console.log('typeof _filename:', typeof __filename);
+console.log('_filename value:', __filename);
+
+console.log('typeof _dirname:', typeof __dirname);
+console.log('_dirname value:', __dirname);
