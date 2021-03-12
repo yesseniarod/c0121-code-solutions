ced69b796b81847444397d384354515c81f07d5e
yesseniarod <yessi.rod30@gmail.com>
2021-03-12 01:54:41 +0000

    Apply code formatting.

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,40 +1,37 @@
-const dataModel = require('./data.json')
+const dataModel = require('./data.json');
 const fs = require('fs');
 
-
-for (let key in dataModel.notes) {
+for (const key in dataModel.notes) {
   if (process.argv[2] === 'read') {
     console.log(`${key}: ${dataModel.notes[key]}`);
   }
 }
 
+if (process.argv[2] === 'create') {
+  const key = dataModel.nextId;
+  dataModel.notes[key] = process.argv[3];
+  dataModel.nextId++;
+  const dataObject = JSON.stringify(dataModel, null, 2);
+  fs.writeFile('data.json', dataObject, 'utf8', err => {
+    if (err) throw err;
+  });
+}
 
-  if (process.argv[2] === 'create') {
-   let key = dataModel.nextId;
-    dataModel.notes[key] = process.argv[3];
-    dataModel.nextId++;
-    let dataObject = JSON.stringify(dataModel, null, 2);
-    fs.writeFile('data.json', dataObject,'utf8', (err) => {
+for (const key in dataModel.notes) {
+  if (process.argv[2] === 'delete' && key === process.argv[3]) {
+    delete dataModel.notes[key];
+    const dataObject = JSON.stringify(dataModel, null, 2);
+    fs.writeFile('data.json', dataObject, 'utf8', err => {
       if (err) throw err;
     });
   }
+}
 
-    for (let key in dataModel.notes) {
-      if (process.argv[2] === 'delete' && key === process.argv[3]) {
-          delete dataModel.notes[key];
-        let dataObject = JSON.stringify(dataModel, null, 2);
-        fs.writeFile('data.json', dataObject, 'utf8', (err) => {
-          if (err) throw err;
-        });
-      }
-    }
-
-
- if (process.argv[2] === 'update') {
-   let key = process.argv[3];
-   dataModel.notes[key] = process.argv[4];
-   let dataObject = JSON.stringify(dataModel, null, 2);
-   fs.writeFile('data.json', dataObject, 'utf8', (err) => {
-     if (err) throw err;
-   });
- }
+if (process.argv[2] === 'update') {
+  const key = process.argv[3];
+  dataModel.notes[key] = process.argv[4];
+  const dataObject = JSON.stringify(dataModel, null, 2);
+  fs.writeFile('data.json', dataObject, 'utf8', err => {
+    if (err) throw err;
+  });
+}
