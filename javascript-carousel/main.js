cc028d552bec490ed4585f91dca1ddd0df05f003
yesseniarod <yessi.rod30@gmail.com>
2021-03-12 06:15:07 +0000

    created new functions for next and previous elements

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -3,42 +3,7 @@ var $previous = document.querySelector('.previous');
 var $currentImage = document.querySelector('.first');
 var $dotContainer = document.querySelector('.dots');
 
-var clickCounter = 0;
-var clickBack = 0;
-
-// $next.addEventListener('click', function (event) {
-//   clickCounter++;
-//   if (clickCounter === 1) {
-//     $currentImage.setAttribute('src', './images/004.png');
-//   } else if (clickCounter === 2) {
-//     $currentImage.setAttribute('src', './images/007.png');
-//   } else if (clickCounter === 3) {
-//     $currentImage.setAttribute('src', './images/025.png');
-//   } else if (clickCounter === 4) {
-//     $currentImage.setAttribute('src', './images/039.png');
-//   } else {
-//     $currentImage.setAttribute('src', './images/001.png');
-//   }
-//   changeImage();
-// });
-
-$previous.addEventListener('click', function (event) {
-  clickBack++;
-  if (clickBack === 1) {
-    $currentImage.setAttribute('src', './images/039.png');
-  } else if (clickBack === 2) {
-    $currentImage.setAttribute('src', './images/025.png');
-  } else if (clickBack === 3) {
-    $currentImage.setAttribute('src', './images/007.png');
-  } else if (clickBack === 4) {
-    $currentImage.setAttribute('src', './images/004.png');
-  } else {
-    $currentImage.setAttribute('src', './images/001.png');
-  }
-});
-
-
-$dotContainer.addEventListener('click', (event) => {
+$dotContainer.addEventListener('click', event => {
   if (event.target && event.target.matches('input.dot1')) {
     $currentImage.setAttribute('src', './images/001.png');
   } else if (event.target && event.target.matches('input.dot2')) {
@@ -57,21 +22,6 @@ var imageSlider = ['./images/001.png', './images/004.png', './images/007.png', '
 var i = 0;
 var imageId = setInterval(changeImage, 3000);
 
-$next.addEventListener('click', function (event) {
-  var currentIndex = 1;
-  if (currentIndex < imageSlider.length) {
-    $currentImage.setAttribute('src', imageSlider[currentIndex]);
-    currentIndex++
-  }
-
-  // debugger;
-  //  changeImage();
-});
-
-// this function will only change image once to charmander
-// no loop should be needed for function
-
-
 function changeImage() {
   i++;
   if (i < imageSlider.length) {
@@ -82,3 +32,22 @@ function changeImage() {
     clearInterval(imageId);
   }
 }
+
+var currentIndex = 0;
+$next.onclick = function () {
+  if (currentIndex >= imageSlider.length - 1) {
+    currentIndex = 0;
+  } else {
+    currentIndex++;
+  }
+  $currentImage.setAttribute('src', imageSlider[currentIndex]);
+};
+
+$previous.onclick = function () {
+  if (currentIndex <= 0) {
+    currentIndex = imageSlider.length - 1;
+  } else {
+    currentIndex--;
+  }
+  $currentImage.setAttribute('src', imageSlider[currentIndex]);
+};
