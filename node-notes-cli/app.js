0fee358a4d78d790926d4b0a658e658f9eb88a3f
yesseniarod <yessi.rod30@gmail.com>
2021-03-13 01:46:15 +0000

    removed unnecessary loop

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -17,15 +17,16 @@ if (process.argv[2] === 'create') {
   });
 }
 
-for (const key in dataModel.notes) {
-  if (process.argv[2] === 'delete' && key === process.argv[3]) {
-    delete dataModel.notes[key];
-    const dataObject = JSON.stringify(dataModel, null, 2);
-    fs.writeFile('data.json', dataObject, 'utf8', err => {
-      if (err) throw err;
-    });
-  }
+// for (const key in dataModel.notes) {
+if (process.argv[2] === 'delete') {
+  const key = process.argv[3];
+  delete dataModel.notes[key];
+  const dataObject = JSON.stringify(dataModel, null, 2);
+  fs.writeFile('data.json', dataObject, 'utf8', err => {
+    if (err) throw err;
+  });
 }
+// }
 
 if (process.argv[2] === 'update') {
   const key = process.argv[3];
