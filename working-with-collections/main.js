d4ee3d67f73a440b5f2800101d7d4e162530272a
yesseniarod <yessi.rod30@gmail.com>
2021-03-15 05:03:10 +0000

    made function to deal two cards to each player, working on scoring game.

--- working-with-collections/main.js
+++ working-with-collections/main.js
@@ -22,8 +22,7 @@ var players = [
   }
 ];
 
-
-function createDeck () {
+function createDeck() {
   var rank = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
   var suit = ['clubs', 'diamonds', 'hearts', 'spades'];
   var cardDeck = [];
@@ -36,35 +35,35 @@ function createDeck () {
   return cardDeck;
 }
 
-function shuffleCards (cardDeck) {
+function shuffleCards(cardDeck) {
 
-for (var x = 0; x < cardDeck.length; x++) {
-  var currentCard = cardDeck[x];
-  var randomCard = Math.floor(Math.random() * cardDeck.length);
-  cardDeck[x] = cardDeck[randomCard];
-  cardDeck[randomCard] = currentCard;
+  for (var x = 0; x < cardDeck.length; x++) {
+    var currentCard = cardDeck[x];
+    var randomCard = Math.floor(Math.random() * cardDeck.length);
+    cardDeck[x] = cardDeck[randomCard];
+    cardDeck[randomCard] = currentCard;
   }
 }
 
 var cards = createDeck();
 shuffleCards(cards);
-// console.log(cards);
 
-// deal two cards to each player
-// make this into a function to reduce repetition
+players.forEach((element, index, array) => {
+  element.hand = cards.splice(0, 2);
+});
 
-players[0].hand = cards.splice(0,2);
-// console.log(players[0].hand);
+// Find the winner
+// create function to determine what player has a better hand
+// rough draft function
 
-players[1].hand = cards.splice(0,2);
-// console.log('player2:', players[1].hand);
-
-players[2].hand = cards.splice(0, 2);
-// console.log('player3:', players[2].hand);
-
-players[3].hand = cards.splice(0, 2);
-// console.log('player4:', players[3].hand);
-
-
-
-// no two players should have the same cards
+// function findWinner(rank) {
+//   var points = 0;
+//   if (rank === 'A') {
+//     points = 11;
+//   } else if (rank === 'J' || rank === 'Q' || rank === 'K') {
+//     points = 10;
+//   } else {
+//     Number(rank);
+//   }
+//   return points;
+// }
