ebdb4c3c5915829289d97e1d91f19ac81e2f9baa
yesseniarod <yessi.rod30@gmail.com>
2021-03-16 22:41:55 +0000

    Completed express-post-json

---
+++ express-post-json/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const nextId = 1;
+const grades = {};
+
+app.get('/api/grades', (req, res) => {
+  var gradesArray = [];
+  for (var key in grades) {
+    gradesArray.push(grades[key]);
+  }
+  res.json(gradesArray);
+});
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Express server listening on port 3000');
+});
+
+const data = express.json();
+app.use(data);
+
+app.post('/api/grades', (req, res) => {
+  req.body.id = nextId;
+  res.send(req.body);
+  res.status(201);
+  grades[0] = req.body;
+});
