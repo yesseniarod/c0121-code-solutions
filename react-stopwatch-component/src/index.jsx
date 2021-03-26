c354761246e4844851d616ed86cd305d94cfd9bd
yesseniarod <yessi.rod30@gmail.com>
2021-03-26 16:20:08 +0000

    Completed react-stopwatch-component

---
+++ react-stopwatch-component/src/index.jsx
@@ -0,0 +1,8 @@
+import StopWatch from './stopwatch';
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+ReactDOM.render(
+  <StopWatch />,
+  document.querySelector('#root')
+);
---
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -0,0 +1,125 @@
+import React from 'react';
+
+class StopWatch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startTime: 0,
+      isPlaying: false,
+      isPaused: true
+    };
+    this.tick = this.tick.bind(this);
+    this.pauseTick = this.pauseTick.bind(this);
+    this.resetTick = this.resetTick.bind(this);
+    this.changeIcon = this.changeIcon.bind(this);
+  }
+
+  tick() {
+    this.intervalId = setInterval(() => {
+      this.setState({
+        startTime: this.state.startTime + 1,
+        isPlaying: true,
+        isPaused: false
+      });
+    }, 1000);
+  }
+
+  pauseTick() {
+    clearInterval(this.intervalId);
+  }
+
+  resetTick() {
+    this.setState({
+      startTime: 0,
+      isPlaying: false,
+      isPaused: true
+    });
+  }
+
+  changeIcon() {
+    if (!this.state.isPlaying) {
+      this.setState({
+        startTime: this.state.startTime,
+        isPlaying: true,
+        isPaused: false
+      });
+      return (
+        <div>
+          <div className='stopwatch-face'>
+            <h1 onClick={this.resetTick}>{this.state.startTime}</h1>
+          </div>
+          <div className='controlButton'>
+            <i className="fas fa-pause" onClick={this.pauseTick}></i>
+          </div>
+        </div>
+      );
+    } else {
+      this.setState({
+        startTime: this.state.startTime,
+        isPlaying: false,
+        isPaused: true
+      });
+      return (
+        <div>
+          <div className='stopwatch-face'>
+            <h1 onClick={this.resetTick}>{this.state.startTime}</h1>
+          </div>
+          <div className='controlButton'>
+            <i className="fas fa-play" onClick={() => {
+              this.tick();
+              this.changeIcon();
+            }}></i>
+          </div>
+        </div>
+      );
+    }
+  }
+
+  render() {
+    if (!this.state.isPlaying) {
+      return (
+        <div>
+          <div className='stopwatch-face'>
+            <h1 onClick={this.resetTick}>{this.state.startTime}</h1>
+          </div>
+          <div className='controlButton'>
+            <i className="fas fa-play" onClick={() => {
+              this.tick();
+              this.changeIcon();
+            }}></i>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <div className='stopwatch-face'>
+            <h1>{this.state.startTime}</h1>
+          </div>
+          <div className='controlButton'>
+            <i className="fas fa-pause" onClick={() => {
+              this.pauseTick();
+              this.changeIcon();
+            }}></i>
+          </div>
+        </div>
+      );
+    }
+  }
+
+}
+
+export default StopWatch;
+
+// else {
+//   return (
+//     <div>
+//       <div className='stopwatch-face'>
+//         <h1 onClick={this.resetTick}>{this.state.startTime}</h1>
+//       </div>
+//       <div className='controlButton'>
+//         <button onClick={this.tick}>start</button>
+//       </div>
+//     </div>
+//   );
+// }
